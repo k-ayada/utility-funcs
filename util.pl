32643d315f10a182781e573c8b2d1c33086827ba
k-ayada <kiran14n@gmail.com>
2018-06-26 18:08:17 +0530

    Update util.pl

--- util.pl
+++ util.pl
@@ -27,3 +27,42 @@ sub toNum {
     $s = "-".substr($s,1,-1) if (substr($s,-1)  eq '-');
     return trim($s);
 }
+sub SUB_RPT_N_DIE {
+ print "$_[0]";
+ exit (0);
+}
+sub SUB_ORA_GET_CONNECTION {
+ my $ora_sid   = $_[0]; #  database SID
+ my $ora_host  = $_[1]; #  database host
+ my $ora_user  = $_[2];
+ my $ora_pswd  = $_[3];
+ my $driver= "Oracle";
+ my $dsn = "DBI:$driver:sid=$$ora_sid;host=$ora_host";
+$dbh = DBI->connect($dsn, $ora_user, $ora_pswd);
+     $ora_conn = ISIS::AC->ac_conn( ( "dbi:$driver:" . $ora_sid ) , "/")
+                 or SUB_RPT_N_DIE( 2, "Failed to get Connection. ".
+                                      "ORA_Err :- $DBI::errstr" );
+ }
+ sub SUB_ORA_PREP_SQL_TEXT {
+ my $ora_conn = $_[0];
+ my $sql_text = $_[1];
+    $ora_stmt = $ora_conn->prepare($sql_text)
+                or SUB_RPT_N_DIE("Failed to prepare the SQl text. ".
+                                    "ORA_Err :- $DBI::errstr" );
+
+ }
+
+ sub SUB_ORA_RUN_STMT {
+ my $ora_stmt = $_[0];
+    $ora_stmt->execute()
+               or SUB_RPT_N_DIE("Faile to execute the statement. ".
+                                    "ORA_Err :- $DBI::errstr" );
+ }
+ 
+ sub PRINT_ORA_ROWS {
+ my $ora_stmt = $_[0] 
+ my @res;
+ while(@res = $ora_stmt->fetchrow_array()) { #.......... Loop thru all the rows returned
+   print $res[0];   
+ } 
+ 
